715b91a6c0f5e7c3603ee2c8d583d5fc11b955d9
ShravanRamjathan <ST10247982@vcconnect.edu.za>
2025-09-01 18:03:18 +0200

    Enhance SimpleList with resizing and search features
    - Modified the `Add` method to check for capacity and call `Resize` when needed.
    - Added a private `Resize` method to double the array size and copy existing values.
    - Introduced `ContainsAt` method to find the index of a specified value.
    - Reintroduced `Clear` method to reset list values to default.

--- UrbanSync.Server/DataStructures/SimpleList.cs
+++ UrbanSync.Server/DataStructures/SimpleList.cs
@@ -14,16 +14,40 @@
         }
 
         public void Add(T value) {
-        values.Append(value);
+            if (_size == _count ) {
+             Resize();
+            }
+            _count++;
+            values[_count] = value;
             
         }
+        private void Resize() { 
+            // i was going to use Array.Copy to be efficient as it does more low level work but
+            // there is also Array.Resize which is more tempting, howver I felt that it would do the work for me
+            // hence i did it manually with a loop. 
+            T[] newArray = new T[_size*2];
+            for(int i =  0; i < values.Length; i++) {
+                newArray[i] = values[i];
+            }
+        }
+       public int  ContainsAt(T Value) {
+            if (!Contains(Value)) return -1;
+            int foundIndex = 0;
+            for(int i = 0; i<_size; i++) {
+                if (values[i]!.Equals(Value)) {
+                    foundIndex = i;
+                }
+            }
+            return foundIndex; ;
+        }
+        public bool Contains(T value) {
+        return values.Contains(value);
+        }
+
         public void Clear() {
             for (int i = 0; i < _count; i++) {
                 values[i] = default!;
             }
-        }
-        public bool Contains(T value) {
-        return values.Contains(value);
         }
     }
 }
