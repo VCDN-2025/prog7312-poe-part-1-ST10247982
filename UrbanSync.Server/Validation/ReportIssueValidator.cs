36b09e026a75987c5047cff34dca96afad9d5530
ShravanRamjathan <ST10247982@vcconnect.edu.za>
2025-09-01 20:08:03 +0200

    Add validation with FluentValidation and new validators
    Updated `UrbanSync.Server.csproj` to include package references for `FluentValidation`, `Microsoft.AspNetCore.Authentication.JwtBearer`, `Microsoft.AspNetCore.OpenApi`, and `Microsoft.AspNetCore.SpaProxy`.
    
    Added `ReportIssueValidator` for validating `ReportedIssue` model fields and `UserValidator` for validating `User` model fields, enhancing data integrity and security during user registration.

---
+++ UrbanSync.Server/Validation/ReportIssueValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UrbanSync.Server.Models;
+
+namespace UrbanSync.Server.Validation {
+    public class ReportIssueValidator:AbstractValidator<ReportedIssue> {
+        public ReportIssueValidator() {
+            RuleFor(report=> report.Id).NotEmpty().NotEmpty();
+            RuleFor(report=>report.UserId).NotEmpty().NotNull();
+            RuleFor(report=>report.Description).MinimumLength(10);
+            RuleFor(report=> report.Image).NotEmpty();
+        }
+    }
+}
---
+++ UrbanSync.Server/Validation/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace UrbanSync.Server.Validation {
+    using FluentValidation;
+    using UrbanSync.Server.Models;
+
+    public class UserValidator : AbstractValidator<User> {
+        public UserValidator() {
+            RuleFor(user => user.Username).NotEmpty().NotNull().MinimumLength(4);
+            RuleFor(user => user.PasswordHash)
+    .NotEmpty().NotNull().WithMessage("Password is required.")
+    .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+    .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+    .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+    .Matches("[0-9]").WithMessage("Password must contain at least one number.")
+    .Matches("[!@#$%^&*()_+=\\[\\]{};':\"\\\\|,.<>/?~]").WithMessage("Password must contain at least one special character.");
+            RuleFor(user => user.Email).EmailAddress().NotNull();
+            RuleFor(user => user.DateOfRegistrstion).NotEmpty().NotNull();
+            RuleFor(user => user.Name).NotEmpty().MinimumLength(3).NotNull();
+
+        }
+    }
+}
